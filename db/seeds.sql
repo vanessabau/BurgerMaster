2bc33988bcf6afc3ef066ca235854e43c6ada616
Vanessa Bautista <vbautista916@icloud.com>
2020-07-26 18:39:16 -0700

    updated burger logo

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name) VALUES ('Lamb Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Bacon', true);
+INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
